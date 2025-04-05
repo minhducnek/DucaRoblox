c1a5288d22806e82c834ba843941482cfd1bc6ad
minhducnek <duc38447@gmail.com>
2025-04-05 19:28:35 +0700

    Update ScriptDuca.lua

--- ScriptDuca.lua
+++ ScriptDuca.lua
@@ -5,6 +5,20 @@ loadstring(game:HttpGet(("https://raw.githubusercontent.com/daucobonhi/Ui-Redz-V
          Title = "Duca Roblox | Script Tổng Hợp BF",
          Animation = "Youtube : Duca Roblox"
          },
+       Key = {
+        KeySystem = false,
+        Title = "Key System",
+        Description = "",
+        KeyLink = "",
+        Keys = {"1234"},
+        Notifi = {
+        Notifications = true,
+        CorrectKey = "Running the Script...",
+       Incorrectkey = "The key is incorrect",
+       CopyKeyLink = "Copied to Clipboard"
+      }
+    }
+  })
        MinimizeButton({
        Image = "https://www.roblox.com/asset/?id=83190276951914",
        Size = {60, 60},
